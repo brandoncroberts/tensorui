2b20c89bf40376dd7f5b0229f4628024a441afe4
Brandon Roberts <8brandon@protonmail.com>
2022-04-15 23:42:22 +0200

    feat: add sort order eslint rule

--- apps/website/specs/index.spec.tsx
+++ apps/website/specs/index.spec.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-
 import Index from '../pages/index';
+
+import { render } from '@testing-library/react';
 
 describe('Index', () => {
   it('should render successfully', () => {
